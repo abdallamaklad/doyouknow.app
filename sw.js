3c454a22bc02f017906b7cc4663594ef6d882148
abdallamaklad <abdalla.maklad@gmail.com>
2026-08-01 09:18:48 +0400

    Fix newsletter forms hidden by critical CSS

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy with stale-while-revalidate for HTML
    ============================================ */
 
-const CACHE_NAME = 'dyk-ea1e44957bea';
+const CACHE_NAME = 'dyk-7687368d8610';
 
 // Core shell pages and assets
 const CORE_URLS = [
